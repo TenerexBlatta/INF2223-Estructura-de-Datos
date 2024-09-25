45566b3fc355f9faf1d77d6587b878ad3b99152b
Tenerex Blatta <125072737+TenerexBlatta@users.noreply.github.com>
2024-09-25 18:05:30 -0300

    Update main.c

--- Ensayo Prueba de Catedra 2/main.c
+++ Ensayo Prueba de Catedra 2/main.c
@@ -1,6 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define MaxVentas 
+#define MaxProductos
 
+struct Juaguey {
+  char *nombreGerente;
+  struct NodoProducto * headProductos;
+  struct NodoSucursal * headSucursales;
+};
+
+struct NodoProducto {
+// Lista Circular
+  struct Producto * producto;
+  struct NodoProducto * sig;
+};
+
+struct Producto {
+  int codigo;
+  char *nombre;
+  float valor;
+  char *tipo;
+
+};
+
+struct NodoSucursal {
+  struct NodoSucursal *ant, *sig;
+  struct Sucursal *sucursal;
+};
+
+struct Sucursal {
+  int id;
+  char *pais;
+  char *encargado;
+  struct Venta ** ventas;
+
+};
+
+struct Venta{
+  int codigo;
+  char *fecha;
+  struct Producto **productos;
+};
+
+struct Sucursal * quitarSucursalConMenosVentas(struct Juaguey * Hw) {
+  struct NodoSucursal *rec = HW->headSucursales;
+  struct NodoSucursal *sucursalMenosVentas = NULL;
+  float menorVenta = -1;
+  float totalVenta;
+
+  if (HW == NULL || HW->headSucursales == NULL {
+    return NULL;
+  }
+
+  while(rec != NULL) {
+    totalVenta = calcularValorSucursal(rec->sucursal);
+
+    if(menorVenta == -1 || totalVenta < menorVenta) {
+      sucursalMenosVentas = rec;
+      menorVenta = totalVenta;
+    }
+    rec = rec->sig;
+  }
+  if(rec->ant != NULL){
+    rec->ant->sig = rec->sig;
+  } else {
+    HW->headSucursales = rec->sig;
+  }
+  
+}
 
 
 
