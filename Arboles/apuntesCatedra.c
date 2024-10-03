49f37d1d8f35c24430e9973c83a2e27d93107de5
Tenerex Blatta <125072737+TenerexBlatta@users.noreply.github.com>
2024-10-03 14:51:35 -0300

    Create apuntesCatedra.c

---
+++ Arboles/apuntesCatedra.c
@@ -0,0 +1,25 @@
+#include <std.io>
+#include <std.lib>
+
+/*
+Arboles Binarios
+
+Repasamos la nomenclatura de sus elementos.
+
+Un arbol equilibrado presenta sus aristas finales en el mismo nivel.
+
+- Se dice que un arbol es degenerado cuando solo tomo un lado como ruta, sus cualidades son: posee un parecido a una lista en como
+  como se  distribuyen sus nodos
+
+- Un arbol lleno, es cual tiene todas sus hojas en el mismo nivel.
+
+  */
+
+/* Tipos de recorrido: 
+- Pre-Orden (Visitas nodo raiz, despues recorres lado izquierdo, y finalmente recorres lado derecho.)
+- In-Orden ()
+- Post-Orden ()
+*/
+
+// Aprender el uso de pila.
+
